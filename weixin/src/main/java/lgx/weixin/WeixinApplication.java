b273320f1e65837f71dd2fc8510cc689e804ed59
admin <admin@DESKTOP-ICF321S>
2019-05-15 09:44:52 +0800

    消息队列

--- weixin/src/main/java/lgx/weixin/WeixinApplication.java
+++ weixin/src/main/java/lgx/weixin/WeixinApplication.java
@@ -1,10 +1,37 @@
 package lgx.weixin;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.util.xml.StaxUtils;
+
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+
+import lgx.weixin.domain.InMessage;
+import lgx.weixin.service.JsonRedisSerializer;
 
 @SpringBootApplication
 public class WeixinApplication {
+	@Bean()
+	public XmlMapper xmlMapper() {
+		XmlMapper mapper = new XmlMapper(StaxUtils.createDefensiveInputFactory());
+		return mapper;
+	}
+	
+	@Bean 
+	public RedisTemplate<String, ? extends InMessage> inMessageTemplate(//
+			
+			@Autowired RedisConnectionFactory connectionFactory) {
+
+		RedisTemplate<String, ? extends InMessage> template = new RedisTemplate<>();
+		template.setConnectionFactory(connectionFactory);
+		template.setValueSerializer(new JsonRedisSerializer<InMessage>());
+
+		return template;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(WeixinApplication.class, args);
--- weixin/src/main/java/lgx/weixin/service/impl/MessageServiceImpl.java
+++ weixin/src/main/java/lgx/weixin/service/impl/MessageServiceImpl.java
@@ -1,6 +1,9 @@
 package lgx.weixin.service.impl;
 
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 import lgx.weixin.controller.MessageReceiverController;
@@ -10,9 +13,15 @@ import lgx.weixin.service.MessageService;
 @Service
 public class MessageServiceImpl implements MessageService {
 	private static final org.slf4j.Logger LOG = LoggerFactory.getLogger(MessageServiceImpl.class);
+	@Autowired
+	@Qualifier("inMessageTemplate")
+	private RedisTemplate<String, ? extends InMessage> inMessageTemplate;
 	@Override
 	public OutMessage onMessage(InMessage msg) {
 		LOG.trace("转化后的消息对象:\n{}\n", msg);
+		//特征   kemao_3_ 通道
+		inMessageTemplate.convertAndSend("kemao_3_" + msg.getMsgType(), msg);
+		
 		return null;
 	}
 
